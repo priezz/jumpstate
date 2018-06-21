b82f0a2f3d56bc6362d5e18ac4adcb001887a534
Alexandr Priezzhev <priezz@users.noreply.github.com>
2018-06-22 01:41:50 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,18 @@
-type IInitialState<S> = {
-  initial: S;
-};
+interface IInitialState <S> {
+    initial: S
+}
 
-export type IReducer<A> = {
-  actionCreators: {
-    [P in keyof A]: A[P];
-  };
-  actionTypes: {
-    [P in keyof A]: P;
-  }
-};
+export type IReducer < A > = {
+    actionCreators: {
+        [P in keyof A]: A[P];
+    };
+    actionTypes: {
+        [P in keyof A]: P;
+    }
+}
 
-type IReduce<S> = (state: S, payload?: any) => S;
-
+type IReduce<S> = (state: S, payload?: any) => S
+// type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 
 /**
  * A jumpstate sandboxed state object that returns a reducer with typed actionCreators and actionTypes
@@ -22,9 +22,19 @@ type IReduce<S> = (state: S, payload?: any) => S;
  * @param name string namespace for the sandbox
  * @param config object defines the reducers and intial state
  */
+
 export type IState = {
-  <A, S>(name: string, config: {[P in keyof A]: IReduce<S>} & IInitialState<S>): IReducer<A>;
-};
+    <A, S>(
+        name: string,
+        config: IInitialState<S> & {
+            [P in keyof A]: P extends keyof IInitialState<S> ? S : IReduce<S>
+        }
+    ): IReducer<A>;
+}
 
+export const State : IState
 
-export const State: IState;
+export const Actions: any
+export const Effect: any
+export const getState: any
+export const CreateJumpstateMiddleware: any
